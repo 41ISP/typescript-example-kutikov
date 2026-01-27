eac0f894b0082c6097156f8f92878da08d6dcc29
Pasha <39154112+ktkv419@users.noreply.github.com>
2026-01-27 05:42:38 +0000

    123

--- expenses/src/App.tsx
+++ expenses/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Form, type TCategory } from './components/Form'
+import { ExpenseCard } from './components/ExpenseCard'
 
 interface IExpense {
   id: number,
@@ -37,7 +38,7 @@ function App() {
       <Form />
       <div className="total">Total: $42.50</div>
       {/* Expense List */}
-      
+      {expenses.map((el) => <ExpenseCard />)}
     </div>
 
   )
